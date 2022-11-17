c81d9305636de189061d357d0a215cc8af1d41fe
ctrl-alt-maxx <maxxr0702@gmail.com>
2022-11-17 15:46:04 -0500

    trying to make the game screen transition

--- game.py
+++ game.py
@@ -1,3 +1,43 @@
 import tkinter as tk
 from tkinter import BOTH, Canvas, Label, PhotoImage
 
+root = tk.Tk()
+
+
+class GameScreen:  
+  
+    def __init__(self, master):
+        self.master = master
+        
+        self.score = 0
+        
+        self.canvas = tk.Canvas(self.master, width=700, height=800, bg="black")
+        self.canvas.pack()
+        
+        self.button2 = tk.Button(self.master, text="Quitter", command=self.master.destroy, font=("Arial", 20), bg="white")
+        self.button2.place( x= 293, y= 500)
+        
+        self.label = tk.Label(self.master, text="Score : ", font=("Arial", 20), bg="white")
+        self.label.pack()
+        
+        self.label = tk.Label(self.master, text="Temps : ", font=("Arial", 20), bg="white")
+        self.label.pack()
+        
+        self.label = tk.Label(self.master, text="Niveau : ", font=("Arial", 20), bg="white")
+        self.label.pack()
+        
+        self.label = tk.Label(self.master, text="Vie : ", font=("Arial", 20), bg="white")
+        self.label.pack()
+        
+        self.label = tk.Label(self.master, text="STARFIGHTER", font=("Arial", 30), bg="white")
+        self.label.pack()
+        
+    def start(self):
+        #self.draw()
+        self.master.mainloop()
+            
+        
+if __name__ == "__main__":
+        app = GameScreen(root)
+        app.start()
+        
--- gameSC.py
+++ gameSC.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import game as g
 from tkinter import BOTH, Canvas, Label, PhotoImage,BOTTOM,RIGHT,LEFT, TOP, X, Y, messagebox
 
 
@@ -8,7 +9,7 @@ root = tk.Tk()
 root.config(background="black")
 
 # Ajouter un titre à la fenêtre Tk
-root.title("Jeu du carré rouge")
+root.title("STARFIGHTER")
 
 # Fixe la taille de la fenêtre en px
 root.geometry("700x800")
@@ -38,53 +39,34 @@ class StartScreen:
         self.button3 = tk.Button(self.master, text="Règles", command=self.master.destroy, font=("Arial", 20), bg="white")
         self.button3.place( x= 291, y= 400)
         
-        self.button1 = tk.Button(self.master, text="Jouer", command=startGame(), font=("Arial", 20), bg="white")
+        self.button1 = tk.Button(self.master, text="Jouer", font=("Arial", 20), bg="white")
         self.button1.place( x= 300, y= 300)
       
         self.limg.place( x= 230, y= 60)
         
-        def startGame(self):
-            self.master.destroy()
-            GameScreen(self.master)    
+           
         
       
     
-        #self.button1.pack(side=tk.BOTTOM
-         
-        
-   
-    
+  
     def start(self):
         #self.draw()
         self.master.mainloop()
+        
+# # def startGame(self):
+#     self.master.destroy()
+#     game = g.GameScreen(root) 
+#     game.start()
 
-class GameScreen: 
-    def __init__(self,master):
-        self.master = master
-        self.master.config(background="black")
-        self.master.title("Starfighter")
-        self.master.geometry("700x800")
-        self.canvas = Canvas(self.master, width=700, height=800, bg="black")
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.label = tk.Label(self.master, text="STARFIGHTER", font=("Arial", 30), bg="white")
-        self.label.pack()
-        self.button1 = tk.Button(self.master, text="Quitter", command=self.master.destroy, font=("Arial", 20), bg="white")
-        self.button1.pack(side=tk.BOTTOM)
-        self.button2 = tk.Button(self.master, text="Rejouer", command=self.master.destroy, font=("Arial", 20), bg="white")
-        self.button2.pack(side=tk.BOTTOM)
-        
-        
-        
-         
-        
-        
-    
-    # if __name__ == "__main__":
-    #     app = StartScreen(root)
-    #     app.start()
-    
-    
-   
 
-   
+
+    
+if __name__ == "__main__":
+        app = StartScreen(root)
+        app.start()
+        
+        
 root.mainloop()
+    
+
+
