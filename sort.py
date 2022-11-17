e4b521876d944001302529bc1aa9bb59e09303ca
LayzyNoob <78440748+LayzyNoob@users.noreply.github.com>
2022-11-17 17:35:03 -0500

    Add files via upload

--- sort.py
+++ sort.py
@@ -5,7 +5,7 @@ import datetime as dt
 
 root = tk.Tk()
 root.title("Score Table")
-root.geometry("800x600")
+root.geometry("1000x1000")
 
 # name = "Marc"
 # pts = random.randrange(100,1000)
@@ -30,32 +30,20 @@ with open("StarFighter.csv", "r", newline="\n") as scorefile:
     reader = csv.reader(scorefile)
     data = list(reader)
     
-    
-    
-scoreslist = []
-for i, row in enumerate(data):
-    scoreslist.append(row[0])
-    for col in range(0, 5):
-        tk.Label(root, text=row[col]).grid(row=i+1, column=col, padx=100)      
-# root.mainloop()   
-    
 def trier(score):
-    pts = 1000000 - int(score[1])
-    battu = 500 - int(score[2])
-    return (pts, battu)    
+    pts = 10000 - int(score[1])
+    # battu = 500 - int(score[2])
+    return (pts)    
     
 Scoreslist = sorted(data, key=trier)
-# print(scoreslist, end='\n\n')
 
-score = tk.Tk()
-score.title("Score Trier")
-score.geometry("800x400")
-
+scoreslist = []
 for i, row in enumerate(Scoreslist):
     scoreslist.append(row[0])
     for col in range(0, 5):
-        tk.Label(score, text=row[col]).grid(row=i+1, column=col, padx=100)  
+        tk.Label(root, text=row[col]).grid(row=i+1, column=col, padx=100)  
+    
 
-score.mainloop()
+# print(scoreslist, end='\n\n')
 
 root.mainloop()
