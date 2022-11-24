3b89f8356b3cd6389dbac863eb1669199b15deae
ctrl-alt-maxx <maxxr0702@gmail.com>
2022-11-24 15:06:53 -0500

    Asteroid object

--- Asteroids.py
+++ Asteroids.py
@@ -22,25 +22,36 @@ frame.pack()
 canvasBase = tk.Canvas(root, background="black", width=800, height=1000, highlightthickness=1, highlightbackground="red", relief='ridge')
 canvasBase.pack()
 
-class Asteroide:
-    imgAsteroide = Image.open("asteroide.png")
-    new = imgAsteroide.resize((60, 60), Image.ANTIALIAS)
-    image = ImageTk.PhotoImage(new)
-    canvasBase.create_image(160, 160, image = image)
+class Asteroids:
+   
     
     def __init__(self, x, y, canvas):
+        
+        imgAsteroide = Image.open("asteroide.png")
+        new = imgAsteroide.resize((60, 60), Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(new)
+     
         
         self.x = x
         self.y = y
         self.canvas = canvas
         self.canvas.create_image(self.x, self.y, image = self.image)
+        
+        self.canvas.after(100, self.move)
+        
+    def move(self):
+        self.canvas.move(self.image, 0, 100)
+        self.canvas.after(100, self.move)
+
+if __name__ == "__main__":
+    Asteroids(130, 160, canvasBase)
+    root.mainloop()
+ 
+
     
 
         
             
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = Asteroids(root)
-    root.mainloop()   
+
     
     
