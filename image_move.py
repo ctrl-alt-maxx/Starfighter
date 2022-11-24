2308d7cff8bf16059534a05ea5fa54d23631db47
ctrl-alt-maxx <maxxr0702@gmail.com>
2022-11-24 17:28:16 -0500

    made an asteroid fall - trying to make more of them at the same time

--- image_move.py
+++ image_move.py
@@ -14,40 +14,38 @@ canvas = Canvas(win, width=600, height=400, bg="white")
 canvas.pack(pady=20)
 
 # Add Images to Canvas widget
-image = ImageTk.PhotoImage(Image.open('logo.png'))
-img = canvas.create_image(250, 120, anchor=NW, image=image)
+# image = ImageTk.PhotoImage(Image.open('asteroide.png'))
+# img = canvas.create_image(0, 120, anchor=NW, image=image)
 
-# def left(e):
-#    x = -20
-#    y = 0
-#    canvas.move(img, x, y)
 
-# def right(e):
-#    x = 20
-#    y = 0
-#    canvas.move(img, x, y)
-
-# def w(e):
-#    x = 0
-#    y = -20
-#    canvas.move(img, x, y)
-
-# def down(e):
-#    x = 0
-#    y = 20
-#    canvas.move(img, x, y)
 
 # Define a function to allow the image to move within the canvas
 def move(root, e):
    global image
-   image = ImageTk.PhotoImage(Image.open('logo.png'))
+   imgAsteroide = Image.open('asteroide.png')
+   new = imgAsteroide.resize((60, 60), Image.ANTIALIAS)
+   image = ImageTk.PhotoImage(new)
    #img = canvas.create_image(e.x, e.y, image=image)
    img = canvas.create_image(e[0], e[1], image=image)
-   root.after(5, partial(move, win, (e[0] - 0, e[1] - 2)))
+   root.after(50, partial(move, win, (e[0] - 0, e[1] + 2)))
 
+def move2(root, e):
+   global image
+   imgAsteroide = Image.open('asteroide.png')
+   new = imgAsteroide.resize((60, 60), Image.ANTIALIAS)
+   image2 = ImageTk.PhotoImage(new)
+   #img = canvas.create_image(e.x, e.y, image=image)
+   img = canvas.create_image(e[0], e[1], image=image2)
+   root.after(50, partial(move, win, (e[0] - 0, e[1] + 2)))
 # Bind the move function
 #canvas.bind("<B1-Motion>", move)
 
-win.after(500, partial(move, win, (240, 120)))
-
+#win.after(5, partial(move, win, (240, -100)))
+def moving():
+   win.after(5, partial(move, win, (240, -100)))
+   win.after(5, partial(move2, win, (240, -100)))
+   
+# move(win, (240, -100))
+# move2(win, (200, -100))
+moving()
 win.mainloop()
