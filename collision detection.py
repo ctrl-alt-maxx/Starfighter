084d364f4d1ac4573f95ed863a884d2f3d93d58c
LayzyNoob <78440748+LayzyNoob@users.noreply.github.com>
2022-12-14 22:16:07 -0500

    ... NOPE

--- collision detection.py
+++ collision detection.py
@@ -16,58 +16,64 @@ canvas.pack(pady=20)
 
 item = ob.Objet.bolt
 
-imgItem = Image.open(item)
-new = imgItem.resize((60, 60), Image.ANTIALIAS)
-image = ImageTk.PhotoImage(new)
-#img = canvas.create_image(e.x, e.y, image=image)
-img = canvas.create_image(300, 400, image=image)
+# imgItem = Image.open(item)
+# new = imgItem.resize((60, 60), Image.ANTIALIAS)
+# image = ImageTk.PhotoImage(new)
+# #img = canvas.create_image(e.x, e.y, image=image)
+# img = canvas.create_image(300, 400, image=image)
 # win.after(50, partial(move, win, (e[0] - 0, e[1] + 5))) # oringinal: 2
 
-# def move(root, e):
-#     global image
-#     imgItem = Image.open(item)
-#     new = imgItem.resize((60, 60), Image.ANTIALIAS)
-#     image = ImageTk.PhotoImage(new)
-#     #img = canvas.create_image(e.x, e.y, image=image)
-#     img = canvas.create_image(e[0], e[1], image=image)
-#     root.after(50, partial(move, win, (e[0] - 0, e[1] + 5)))
-#     collision(img)
+def move(root, e):
+    global image
+    imgItem = Image.open(item)
+    new = imgItem.resize((60, 60), Image.ANTIALIAS)
+    image = ImageTk.PhotoImage(new)
+    img = canvas.create_image(e[0], e[1], image=image)
+    root.after(50, partial(move, win, (e[0] - 0, e[1] + 5)))
+    collision(img)
 
 imageVaisseau = Image.open("vaisseau.png")
 test = ImageTk.PhotoImage(imageVaisseau)
 
+
 #Resize the Image using resize method
-# resized_image = imageVaisseau.resize((50,50), Image.ANTIALIAS)
-# new_image = ImageTk.PhotoImage(resized_image)
+resized_image = imageVaisseau.resize((50,50), Image.ANTIALIAS)
+new_image = ImageTk.PhotoImage(resized_image)
+vai = canvas.create_image(500, 400, image=new_image)
 
 # Define a function to allow the image to move within the canvas 
 def moveV(e):
     global image2
-    new = imageVaisseau.resize((50,50), Image.ANTIALIAS)
-    image2 = ImageTk.PhotoImage(new)
+    # new = imageVaisseau.resize((50,50), Image.ANTIALIAS)
+    image2 = ImageTk.PhotoImage(resized_image)
     img = canvas.create_image(e.x, e.y, image = image2)
-    collision(img)
+    # return img
+    # collision(img)
     
 
 # Bind the move function 
 canvas.bind("<Motion>", moveV)  ## THIS WORKS!! vaisseau follows the cursor
 
+#v = moveV
+
 def collision(objet):
-    sb = canvas.bbox(objet)
-    eb = canvas.bbox(img)
+    sb = canvas.bbox(vai)
+    eb = canvas.bbox(objet)
     if eb[0] < sb[2] < eb[2] and eb[1] < sb[1] < eb[3]:
-        canvas.move(img, 25, 25)
+        canvas.move(objet, 25, -25)
         print("CONTACT BOTTOM-LEFT")
     elif eb[2] > sb[0] > eb[0] and eb[1] < sb[1] < eb[3]:
-        canvas.move(img, -25, -25)
+        canvas.move(objet, -25, -25)
         print("CONTACT BOTTOM-RIGHT")
-    # elif eb[1] < sb[1] < eb[3] and eb[0] < sb[0] < eb[3]:
-    #     canvas.move(img, 25, -25)
-    #     print("CONTACT TOP")
-    # elif eb[0] < sb[2] < eb[2] and eb[1] > sb[1] > eb[3]:
-    #     canvas.move(img, -25, 25)
-    #     print("CONTACT TOM-LEFT")
+    elif sb[1] < eb[1] < sb[3] and eb[0] < sb[2] < eb[2]:
+        canvas.move(objet, 25, 25)
+        print("CONTACT TOP-RIGHT")
+    elif sb[1] < eb[1] < sb[3] and sb[0] < eb[2] < sb[2]:
+        canvas.move(objet, -25, 25)
+        print("CONTACT TOP-LEFT")
 
-# win.after(5, partial(move, win, (5, 5)))
-
+def moving():
+    win.after(0, partial(move, win, (500, -20)))
+    
+moving()
 win.mainloop()
